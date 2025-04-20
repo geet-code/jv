ce34248a0544bff573fa667d50b251df1f33babf
geet-code <geet_asrani.git@outlook.com>
2025-04-20 17:35:05 +0530

    msg

--- pdf4/Q4.java
+++ pdf4/Q4.java
@@ -5,26 +5,26 @@ package pdf4;
 import java.util.Scanner;
 public class Q4 {
    public static void main(String[] args) {
-    //    Scanner sc = new Scanner(System.in);
-    //    System.out.print("Enter size of array: ");
-    //    int n = sc.nextInt();
-    //    int[] arr = new int[n];
-    //    System.out.println("Enter elements:");
-    //    for (int i = 0; i < n; i++) {
-    //        arr[i] = sc.nextInt();
-    //    }
-    //    System.out.print("Enter number to search: ");
-    //    int x = sc.nextInt();
-    //    boolean found = false;
-    //    for (int val : arr) {
-    //        if (val == x) {
-    //            found = true;
-    //            break;
-    //        }
-    //    }
-    //    if (found)
-    //        System.out.println(x + " is present in the array.");
-    //    else
-    //        System.out.println(x + " is not present in the array.");
+       Scanner sc = new Scanner(System.in);
+       System.out.print("Enter size of array: ");
+       int n = sc.nextInt();
+       int[] arr = new int[n];
+       System.out.println("Enter elements:");
+       for (int i = 0; i < n; i++) {
+           arr[i] = sc.nextInt();
+       }
+       System.out.print("Enter number to search: ");
+       int x = sc.nextInt();
+       boolean found = false;
+       for (int val = 0; val < n; val++) { //int val : arr
+           if (arr[val] == x) {
+               found = true;
+               break;
+           }
+       }
+       if (found)
+           System.out.println(x + " is present in the array.");
+       else
+           System.out.println(x + " is not present in the array.");
    }
 }
